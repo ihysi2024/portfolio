53e8346c25e5b3e479a71203524e15154f548a2c
Ira Hysi <irahysi2024@gmail.com>
2025-02-02 23:48:17 -0500

    fixing

--- src/components/Employai.js
+++ src/components/Employai.js
@@ -1,52 +1,46 @@
 import employai_video from "../assets/Employ.ai.mp4";
-import './Employai.css'; // Import the CSS file
+import './Employai.css';
 
 function EmployAI() {
   return (
-    <div className="employai-n">
-      <h1 className="employai-title">Employ.ai</h1>
-      <div className="employai-title-separator"></div>
-      {/* Video */}
-      <video
-        className="employai-recording"
-        src={employai_video}
-        autoPlay
-        muted
-        loop
-        controls
-      />
-
-      {/* Title */}
-
-      {/* Description and Tech Stack */}
-      <div className="employai-bottom">
-        {/* Description */}
-        <div className="employai-desc">
-          This NodeJS application caters available job postings to users based on their preferences and previous work experiences using an AI-driven recommendation algorithm. 
-          Users can enter their past work experiences and optionally include a preferred work location and base salary range, without needing to search for specific positions.
-        </div>
-
-        {/* Tech Stack */}
-        <div>
-          <div style={{width: '40vw'}}>
-            <div className="employai-backend-title">Backend</div>
-            <div className="employai-tech-stack">
-              <div className="employai-backend-item">Javascript</div>
-              <div className="employai-backend-item">Firebase</div>
-            </div>    
-
-            <div className="employai-frontend-title">Frontend</div>
-            <div className="employai-tech-stack">
-              <div className="employai-frontend-item">TailwindCSS</div>
-              <div className="employai-frontend-item">React</div>
-              <div className="employai-frontend-item">Material UI</div>
+    <div className="employai-container">
+      <div className="employai-content">
+        <div className="employai-text">
+          <h1 className="employai-title">Employ.ai</h1>
+          <div className="employai-title-separator"></div>
+          <video 
+            className="employai-recording"
+            src={employai_video}
+            autoPlay
+            muted
+            loop
+            controls
+          />
+          <div className="employai-split-row">
+            <div className="employai-desc">
+              This NodeJS application caters available job postings to users based on their preferences and previous work experiences using an AI-driven recommendation algorithm. 
+              Users can enter their past work experiences and optionally include a preferred work location and base salary range, without needing to search for specific positions.
             </div>
+            <div className="employai-tech-stacks">
+              <h2 className="employai-backend-title">Backend</h2>
+              <div className="employai-tech-stack">
+                <div className="employai-backend-item">Javascript</div>
+                <div className="employai-backend-item">Firebase</div>
+              </div>    
 
-            <div className="employai-tool-title">Tools</div>
-            <div className="employai-tech-stack">
-              <div className="employai-tool-item">OpenAI</div>
-              <div className="employai-tool-item">Adzuna API</div>
-              <div className="employai-tool-item">Vercel</div>
+              <h2 className="employai-frontend-title">Frontend</h2>
+              <div className="employai-tech-stack">
+                <div className="employai-frontend-item">TailwindCSS</div>
+                <div className="employai-frontend-item">React</div>
+                <div className="employai-frontend-item">Material UI</div>
+              </div>
+
+              <h2 className="employai-tool-title">Tools</h2>
+              <div className="employai-tech-stack">
+                <div className="employai-tool-item">OpenAI</div>
+                <div className="employai-tool-item">Adzuna API</div>
+                <div className="employai-tool-item">Vercel</div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -1,46 +1,47 @@
 import pantry_video from "../assets/pantry.mov";
-import './Pantry.css'; 
+import './Pantry.css';
 
 function Pantry() {
   return (
-    <div className="pantry-n">
-      <h1 className="pantry-title">Pantry Tracker</h1>
-      <div className="pantry-title-separator"></div>
+    <div className="pantry-container">
+      <div className="pantry-content">
+        <div className="pantry-text">
+          <h1 className="pantry-title">Pantry Tracker</h1>
+          <div className="pantry-title-separator"></div>
+          <video 
+            className="pantry-recording"
+            src={pantry_video}
+            autoPlay
+            muted
+            loop
+            controls
+          />
+          <div className="pantry-split-row">
+            <div className="pantry-desc">
+              This web application allows users to manage their pantry by tracking items through a responsive pie chart. With the help of AI, users are also able to generate
+              nutritional data about a given item in the pantry and be mindful of their food intake.
+            </div>
+            <div className="pantry-tech-stacks">
+              <h2 className="pantry-backend-title">Backend</h2>
+              <div className="pantry-tech-stack">
+                <div className="pantry-backend-item">Javascript</div>
+                <div className="pantry-backend-item">Firebase</div>
+              </div>    
 
-      <video 
-        className="pantry-recording"
-        src={pantry_video}
-        autoPlay
-        muted
-        loop
-        controls
-      />
+              <h2 className="pantry-frontend-title">Frontend</h2>
+              <div className="pantry-tech-stack">
+                <div className="pantry-frontend-item">CSS</div>
+                <div className="pantry-frontend-item">Material UI</div>
+                <div className="pantry-frontend-item">React</div>
+              </div>
 
-      <div className="pantry-bottom">
-        <div className="pantry-description">
-          This web application allows users to manage their pantry by tracking items through a responsive pie chart. With the help of AI, users are also able to generate
-          nutritional data about a given item in the pantry and be mindful of their food intake.
+              <h2 className="pantry-tool-title">Tools</h2>
+              <div className="pantry-tech-stack">
+                <div className="pantry-tool-item">OpenAI</div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div>
-        <div className="pantry-backend-title">Backend</div>
-        <div className="pantry-tech-stack">
-          <div className="pantry-backend-item">Javascript</div>
-          <div className="pantry-backend-item">Firebase</div>
-        </div>
-        
-        <div className="pantry-frontend-title">Frontend</div>
-        <div className="pantry-tech-stack">
-          <div className="pantry-frontend-item">CSS</div>
-          <div className="pantry-frontend-item">Material UI</div>
-          <div className="pantry-frontend-item">React</div>
-        </div>
-        
-        <div className="pantry-tool-title">Tools</div>
-        <div className="pantry-tech-stack">
-          <div className="pantry-tool-item">OpenAI</div>
-        </div>
-      </div>
       </div>
     </div>
   );
